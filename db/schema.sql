a0573aa3310f66f27d4f7b6271e5be046cf09de4
stellie82 <kims82@gmail.com>
2019-12-05 14:33:38 -0800

    add createdat column in sql file

--- db/schema.sql
+++ db/schema.sql
@@ -9,5 +9,6 @@ CREATE TABLE burgers
 	id INT NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
