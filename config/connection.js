8129dacd295855f1b9cf2cd87e78196a298eae42
stellie82 <kims82@gmail.com>
2019-12-05 14:00:46 -0800

    setup JawsDB provisioning

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+// Set up JawsDB provisioning or fallback on local database
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function (err) {
     if (err) {
